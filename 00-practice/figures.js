760f7747e6dfdd7dfbb44ebfe7e48083a6943830
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-01-19 14:18:02 -0300

    feat (triangle-html): getSides(), calculateTrianglePerimeter() & calculateTriangleArea() functions created. Parsing input values to float.

--- 00-practice/figures.js
+++ 00-practice/figures.js
@@ -46,15 +46,54 @@ console.groupEnd();
 
 // Square <-> HTML
 function getSquareSide() {
-  return document.getElementById('squareSide').value;
+  return parseFloat(document.getElementById('squareSide').value);
 }
 
 function calculateSquarePerimeter() {
   const squareSide = getSquareSide();
+
+  alert(squarePerimeter(squareSide));
   return squarePerimeter(squareSide);
 }
 
 function calculateSquareArea() {
   const squareSide = getSquareSide();
+
+  alert(squareArea(squareSide));
   return squareArea(squareSide);
+}
+
+// Triangle <-> HTML
+function getSides() {
+  let sides = {};
+  const sideA = parseFloat(document.getElementById('triangleSideA').value);
+  const sideB = parseFloat(document.getElementById('triangleSideB').value);
+  const base = parseFloat(document.getElementById('triangleBase').value);
+  const height = parseFloat(document.getElementById('triangleHeight').value);
+
+  sides["sideA"] = sideA;
+  sides["sideB"] = sideB
+  sides["base"] = base;
+  sides["height"] = height;
+
+  return sides;
+}
+
+function calculateTrianglePerimeter() {
+  const sides = getSides();
+  const sideA = sides["sideA"];
+  const sideB = sides["sideB"];
+  const base = sides["base"];
+
+  alert(trianglePerimeter(sideA, sideB, base));
+  return trianglePerimeter(sideA, sideB, base);
+}
+
+function calculateTriangleArea() {
+  const sides = getSides();
+  const base = sides["base"];
+  const height = sides["height"];
+
+  alert(triangleArea(base, height));
+  return triangleArea(base, height);
 }
