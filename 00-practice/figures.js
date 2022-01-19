02a27d706de3dee1de1190f3b939b6b586ae203c
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-01-19 19:59:55 -0300

    feat: triangleExists() & equilateral, isosceles and scalene triangles functions to obtain height & area of each one added. Alert for values == 0 added (sides, radius & diameter). calculateTriangleHeight() function added. Changes on calculateTriangleArea() added. Minor change on calculateCirclePerimeter().

--- 00-practice/figures.js
+++ 00-practice/figures.js
@@ -2,36 +2,91 @@
 // Square code
 const squarePerimeter = side => {
   return side * 4;
-}
+};
 
 const squareArea = side => {
   return side ** 2;
-}
+};
 
 // Triangle code
+const triangleExists = (side1, side2, base) => {
+  return (
+    side1 + side2 > base &&
+    side1 + base > side2 &&
+    side2 + base > side1
+  );
+}
+
 const trianglePerimeter = (side1, side2, base) => {
   return side1 + side2 + base;
-}
+};
 
 const triangleArea = (base, height) => {
   return (base * height) / 2; 
-}
+};
+
+// Equilateral
+const equilateralTriangleHeight = side => {
+  const squaredSide = side ** 2;
+  const squaredNewBase = (side / 2) ** 2;
+  let height = Math.sqrt(squaredSide - squaredNewBase);
+  console.log(height);
+
+  return height;
+};
+
+const equilateralTriangleArea = side => {
+  const height = equilateralTriangleHeight(side);
+
+  return triangleArea(side, height);
+};
+
+// Isosceles
+const isoscelesTriangleHeight = (side, base) => {
+  const newSquaredBase = (base / 2) ** 2;
+  const squaredSide = side ** 2;
+
+  return Math.sqrt(squaredSide - newSquaredBase);
+};
+
+const isoscelesTriangleArea = (side, base) => {
+  const height = isoscelesTriangleHeight(side, base);
+
+  return triangleArea(base, height);
+};
+
+// Scalene
+const scaleneTriangleArea = (sideA, sideB, base) => {
+  // heron's formula
+  const semiPerimeter = (sideA + sideB + base) / 2;
+  const firstTerm = semiPerimeter - sideA;
+  const secondTerm = semiPerimeter - sideB;
+  const thirdTerm = semiPerimeter - base;
+
+  return Math.sqrt(semiPerimeter * firstTerm * secondTerm * thirdTerm);
+};
+
+const scaleneTriangleHeight = (sideA, sideB, base) => {
+  const area = scaleneTriangleArea(sideA, sideB, base);
+
+  return (area * 2) / base;
+};
 
 // Circle code
 const circleDiameter = radius => {
   return radius * 2;
-}
+};
 
 const PI = Math.PI;
 
 const circlePerimeter = radius => {
   const diameter = circleDiameter(radius);
   return diameter * PI;
-}
+};
 
 const circleArea = radius => {
   return (radius ** 2) * PI;
-}
+};
 
 // # Geometric figures <-> HTML #
 // Square <-> HTML
@@ -42,12 +97,22 @@ function getSquareSide() {
 function calculateSquarePerimeter() {
   const squareSide = getSquareSide();
 
+  if (squareSide <= 0) {
+    alert("Side MUST be greather than 0");
+    return;
+  }
+
   alert(squarePerimeter(squareSide));
   return squarePerimeter(squareSide);
 }
 
 function calculateSquareArea() {
   const squareSide = getSquareSide();
+
+  if (squareSide <= 0) {
+    alert("Side MUST be greather than zero.");
+    return;
+  }
 
   alert(squareArea(squareSide));
   return squareArea(squareSide);
@@ -59,14 +124,51 @@ function getSides() {
   const sideA = parseFloat(document.getElementById('triangleSideA').value);
   const sideB = parseFloat(document.getElementById('triangleSideB').value);
   const base = parseFloat(document.getElementById('triangleBase').value);
-  const height = parseFloat(document.getElementById('triangleHeight').value);
 
   sides["sideA"] = sideA;
   sides["sideB"] = sideB
   sides["base"] = base;
-  sides["height"] = height;
 
   return sides;
+}
+
+function calculateTriangleHeight() {
+  let height;
+  const sides = getSides();
+  const sideA = sides["sideA"];
+  const sideB = sides["sideB"];
+  const base = sides["base"];
+
+  if (sideA <= 0 || sideB <= 0 || base <= 0) {
+    alert("ALL sides MUST be greater than zero.");
+    return;
+  }
+
+  if (!triangleExists(sideA, sideB, base)) {
+    alert("That triangle doesn't exist.");
+    return;
+  }
+
+  // Equilateral
+  if (sideA == sideB && sideA == base) {
+    height = equilateralTriangleHeight(sideA);
+  // Isosceles
+  } else if (sideA == sideB && sideA != base) {
+    height = isoscelesTriangleHeight(sideA, base);
+
+  } else if (sideB == base && sideB != sideA) {
+    height = isoscelesTriangleHeight(sideB, sideA);
+
+  } else if (base == sideA && base != sideB) {
+    height = isoscelesTriangleHeight(base, sideB);
+
+  // Scalene
+  } else {
+    height = scaleneTriangleHeight(sideA, sideB, base);
+  }
+
+  alert(height);
+  return height;
 }
 
 function calculateTrianglePerimeter() {
@@ -75,17 +177,57 @@ function calculateTrianglePerimeter() {
   const sideB = sides["sideB"];
   const base = sides["base"];
 
+  if (sideA <= 0 || sideB <= 0 || base <= 0) {
+    alert("ALL sides MUST be greater than zero.");
+    return;
+  }
+
+  if (!triangleExists(sideA, sideB, base)) {
+    alert("That triangle doesn't exist.");
+    return;
+  }
+
   alert(trianglePerimeter(sideA, sideB, base));
   return trianglePerimeter(sideA, sideB, base);
 }
 
 function calculateTriangleArea() {
+  let area;
   const sides = getSides();
+  const sideA = sides["sideA"];
+  const sideB = sides["sideB"];
   const base = sides["base"];
-  const height = sides["height"];
 
-  alert(triangleArea(base, height));
-  return triangleArea(base, height);
+  if (sideA <= 0 || sideB <= 0 || base <= 0) {
+    alert("ALL sides MUST be greater than zero.");
+    return;
+  }
+
+  if (!triangleExists(sideA, sideB, base)) {
+    alert("That triangle doesn't exist.");
+    return;
+  }
+
+  // Equilateral
+  if (sideA == sideB && sideA == base) {
+    area = equilateralTriangleArea(sideA);
+  // Isosceles
+  } else if (sideA == sideB && sideA != base) {
+    area = isoscelesTriangleArea(sideA, base);
+
+  } else if (sideB == base && sideB != sideA) {
+    area = isoscelesTriangleArea(sideB, sideA);
+
+  } else if (base == sideA && base != sideB) {
+    area = isoscelesTriangleArea(base, sideB);
+
+  // Scalene
+  } else {
+    area = scaleneTriangleArea(sideA, sideB, base);
+  }
+
+  alert(area);
+  return area;
 }
 
 // Circle <-> HTML
@@ -96,19 +238,34 @@ function getRadius() {
 function calculateCircleDiameter() {
   const radius = getRadius();
 
+  if (radius <= 0) {
+    alert("Radius MUST be greater than zero.");
+    return 0;
+  }
+
   alert(circleDiameter(radius));
   return circleDiameter(radius);
 }
 
 function calculateCirclePerimeter() {
-  const perimeter = circleDiameter(getRadius());
+  const diameter = circleDiameter(getRadius());
 
-  alert(circlePerimeter(perimeter));
-  return circlePerimeter(perimeter);
+  if (diameter <= 0) {
+    alert("Radius MUST be greater than zero.");
+    return;
+  }
+
+  alert(circlePerimeter(diameter));
+  return circlePerimeter(diameter);
 }
 
 function calculateCircleArea() {
   const radius = getRadius();
+
+  if (radius <= 0) {
+    alert("Radius MUST be greater than zero.");
+    return;
+  }
 
   alert(circleArea(radius));
   return circleArea(radius);
