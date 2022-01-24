f304ecfa12a23efcb55e00a0f64c28f7fcbe0b35
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-01-23 21:47:52 -0300

    feat (js): coupons array created. getPrice(), getDiscount() getCoupon(), priceWithDiscount() & isCouponValid() functions added. calculatePriceWithDiscount() function modified.

--- 00-practice/percentages-discounts.js
+++ 00-practice/percentages-discounts.js
@@ -1,6 +1,60 @@
-function calculatePriceWithDiscount(price, discount) {
+const coupons = [
+  {
+    name: 'summer2022',
+    discount: 15,
+  },
+  {
+    name: 'tkd2022',
+    discount: 30,
+  },
+  {
+    name: 'ltomasdev',
+    discount: 25,
+  },
+];
+
+function getPrice() {
+  return parseFloat(document.getElementById('price').value);
+}
+
+function getDiscount() {
+  return parseFloat(document.getElementById('discount').value);
+}
+
+function getCoupon() {
+  resultCoupon.innerText = "";
+  return document.getElementById('coupon').value;
+}
+
+function priceWithDiscount(price, discount) {
   const percentageToPay = 100 - discount;
   const priceWithDiscount = (price * percentageToPay) / 100;
 
   return priceWithDiscount;
+}
+
+function isCouponValid(coupon) {
+  return coupons.find(e => e.name === coupon.toLowerCase());
+}
+
+let resultPrice = document.getElementById('resultPrice');
+
+function calculatePriceWithDiscount() {
+  const price = getPrice();
+  let discount = getDiscount();
+  const couponToEvaluate = getCoupon().replace(/\s+/g, "");
+
+  if (couponToEvaluate !== "") {
+    const coupon = isCouponValid(couponToEvaluate);
+
+    if (coupon != undefined) {
+      discount += coupon.discount;
+      resultCoupon.innerText = `"${coupon.name}" (-${coupon.discount}%) coupon applied!`;
+    } else {
+      resultCoupon.innerText = `"${couponToEvaluate}" is not a valid coupon.`;
+    }
+  }
+
+  const newPrice = priceWithDiscount(price, discount);
+  resultPrice.innerText = `$ ${price} (-${discount}%) = $ ${newPrice}`;
 }
