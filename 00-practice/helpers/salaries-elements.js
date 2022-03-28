f2d40cd1910d2b25790a96fc7df6ae85b74483fe
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-03-28 20:26:40 -0300

    feat: <salaries-elements.js> gets ROI, Simple interest & Compound interest showing buttons and its div sections.

---
+++ 00-practice/helpers/salaries-elements.js
@@ -0,0 +1,17 @@
+const roiButton = document.getElementById('roi');
+const roiSection = document.querySelector('.roi-section');
+
+const simpleButton = document.getElementById('simple');
+const simpleSection = document.querySelector('.simple-section');
+
+const compoundButton = document.getElementById('compound');
+const compoundSection = document.querySelector('.compound-section');
+
+export {
+  roiButton,
+  roiSection,
+  simpleButton,
+  simpleSection,
+  compoundButton,
+  compoundSection,
+}
