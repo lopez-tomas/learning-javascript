ef6cd95d2c73554899b4c1ec9bf495f9809e1418
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-04 01:03:33 -0300

    feat: Private methods for Classes in JavaScript.

--- 01-ecmascript/src/es12/index.js
+++ 01-ecmascript/src/es12/index.js
@@ -5,3 +5,22 @@ console.log(replacedString);
 
 const replacedString2 = string.replaceAll("JavaScript", "Python");
 console.log(replacedString2);
+
+
+// # Private methods for classes
+class Message {
+  show(value) {
+    console.log(value);
+  }
+  #showPrivate(value) {
+    console.log(value);
+  }
+  
+  get #add(value) {
+    ...
+  }
+}
+
+const message = new Message();
+message.show('Hello!');
+message.showPrivate('Hello!');
