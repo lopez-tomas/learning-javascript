e0a436d581c25b029751a946c4c142088aac8a4c
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-04 01:06:51 -0300

    feat: Promise.any() in JavaScript.

--- 01-ecmascript/src/es12/index.js
+++ 01-ecmascript/src/es12/index.js
@@ -24,3 +24,12 @@ class Message {
 const message = new Message();
 message.show('Hello!');
 message.showPrivate('Hello!');
+
+
+// # Promise.any()
+const promise1 = new Promise((resolve, reject) => reject("1"));
+const promise2 = new Promise((resolve, reject) => resolve("2"));
+const promise3 = new Promise((resolve, reject) => resolve("3"));
+
+Promise.any([promise1, promise2, promise3])
+  .then(response => console.log(response));
