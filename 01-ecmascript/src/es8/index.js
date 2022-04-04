d66b7d7ea6c2d5f91865938b8487ad9fc46e503f
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-03 23:28:44 -0300

    feat: Object.values in JavaScript.

--- 01-ecmascript/src/es8/index.js
+++ 01-ecmascript/src/es8/index.js
@@ -1,10 +1,16 @@
-// # Object.entries: it returns key-value pair of a matrix
 const data = {
   frontend: 'Oscar',
   backend: 'Isabel',
   design: 'Ana',
 }
 
+// # Object.entries: it returns key-value pair of a matrix
 const entries = Object.entries(data);
 console.log(entries);
 console.log(entries.length);
+
+
+// # Object.values: it returns an object values into an array
+const values = Object.values(data);
+console.log(values);
+console.log(values.length);
