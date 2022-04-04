e06d6bc717e34686ff40266099c30c6b780ad6f0
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-03 23:23:32 -0300

    feat: ES8 - Object.entries in JavaScript.

---
+++ 01-ecmascript/src/es8/index.js
@@ -0,0 +1,10 @@
+// # Object.entries: it returns key-value pair of a matrix
+const data = {
+  frontend: 'Oscar',
+  backend: 'Isabel',
+  design: 'Ana',
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
