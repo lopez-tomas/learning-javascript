0b36b07d011891fc42da2c394bf1174392c0f68c
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-03 23:41:34 -0300

    feat: Async Await in JavaScript.

--- 01-ecmascript/src/es8/index.js
+++ 01-ecmascript/src/es8/index.js
@@ -26,3 +26,31 @@ console.log(string.padEnd(12, ' ------'));
 const obj = {
   name: 'Oscar',
 };
+
+
+// # Async Await
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (true)
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Test error'))
+  });
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.err(error);
+  }
+}
+
+anotherFunction();
