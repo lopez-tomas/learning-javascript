0edfc382a80da97b9d2b4736a5392392f5fd5ef9
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-04 00:44:47 -0300

    feat: Promise.allSettled() in JavaScript.

--- 01-ecmascript/src/es11/index.js
+++ 01-ecmascript/src/es11/index.js
@@ -13,3 +13,12 @@ const anotherBigIntNumber = BigInt(9007199254740991);
 
 console.log(aBigNumber);
 console.log(anotherBigIntNumber);
+
+
+// # Promise.allSettled()
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise2 = new Promise((resolve, reject) => resolve("resolve"));
+const promise3 = new Promise((resolve, reject) => resolve("resolve2"));
+
+Promise.allSettled([promise1, promise2, promise3])
+  .then(response => console.log(response));
