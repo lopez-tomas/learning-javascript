00759bf922f1eb057f3343e350c9078cee281996
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-04 00:42:09 -0300

    feat: Big Int(<=2^53) in JavaScript.

--- 01-ecmascript/src/es11/index.js
+++ 01-ecmascript/src/es11/index.js
@@ -5,3 +5,11 @@ button.addEventListener("click", async function () {
   const module = await import("./file.js");
   module.hello();
 });
+
+
+// # Big INT = 2^53
+const aBigNumber = 9007199254740991n;
+const anotherBigIntNumber = BigInt(9007199254740991);
+
+console.log(aBigNumber);
+console.log(anotherBigIntNumber);
