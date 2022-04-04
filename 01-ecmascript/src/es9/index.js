5182f44bfe38d988724131994c43679b6dcbcc1d
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-04 00:11:50 -0300

    feat: Regex grouping in JavaScript.

--- 01-ecmascript/src/es9/index.js
+++ 01-ecmascript/src/es9/index.js
@@ -36,3 +36,13 @@ helloWorld()
   .then(response => console.log(response))
   .catch(error => console.log(error))
   .finally(() => console.log('Completed'));
+
+
+// # Grouping Regex
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+const match = regexData.exec('2018-04-20');
+const year = match[1];
+const month = match[2];
+const day = match[3];
+
+console.log(year, month, day);
