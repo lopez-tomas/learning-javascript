2b98e10db689f1a6664ad8b4ec2bbc38b93d8d5c
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-03 23:31:35 -0300

    feat: Paddings (padStart & padEnd) in JavaScript.

--- 01-ecmascript/src/es8/index.js
+++ 01-ecmascript/src/es8/index.js
@@ -14,3 +14,9 @@ console.log(entries.length);
 const values = Object.values(data);
 console.log(values);
 console.log(values.length);
+
+
+// # Padding
+const string = 'hello';
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(12, ' ------'));
