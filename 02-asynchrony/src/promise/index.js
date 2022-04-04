d4c47e0fc6a566dc3fc2f8711a412395e63ca1af
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-04 14:12:13 -0300

    minor change.

--- 02-asynchrony/src/promise/index.js
+++ 02-asynchrony/src/promise/index.js
@@ -31,6 +31,6 @@ somethingWillHappen2()
   .catch(err => console.error(err));
 
 
-Promise.all([somethingWillHappen, somethingWillHappen2])
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
   .then(response => console.log('Array of results', response))
   .catch(err => console.error(err));
