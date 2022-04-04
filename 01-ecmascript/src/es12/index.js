38b628380ed76a77bf3bd935196ee18c33dcf744
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-04 01:01:03 -0300

    feat: ES12 (ECMAScript 2021) - String.replaceAll() in JavaScript.

---
+++ 01-ecmascript/src/es12/index.js
@@ -0,0 +1,7 @@
+// # replaceAll
+const string = "JavaScript is wonderful! With JavaScript I can create the future of the web.";
+const replacedString = string.replace("JavaScript", "Python");
+console.log(replacedString);
+
+const replacedString2 = string.replaceAll("JavaScript", "Python");
+console.log(replacedString2);
