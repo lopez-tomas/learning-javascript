95237176637eca4a26572d83409f390b1a8831fe
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-04-04 00:08:47 -0300

    feat: Promise.finally() in JavaScript.

--- 01-ecmascript/src/es9/index.js
+++ 01-ecmascript/src/es9/index.js
@@ -21,3 +21,18 @@ const obj3 = {
 }
 
 console.log(obj3);
+
+
+// # Promise.finally
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (true)
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Test error'))
+  });
+};
+
+helloWorld()
+  .then(response => console.log(response))
+  .catch(error => console.log(error))
+  .finally(() => console.log('Completed'));
