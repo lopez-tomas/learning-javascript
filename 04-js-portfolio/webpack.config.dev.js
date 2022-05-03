96a8f608e9fcff66ca359eb673c9de63557e25d6
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-02 21:17:13 -0300

    feat: Bundle Analyzer set in webpack development configuration.

--- 04-js-portfolio/webpack.config.dev.js
+++ 04-js-portfolio/webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin') ;
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: './src/index.js',
@@ -83,6 +84,7 @@ module.exports = {
       ]
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
   devServer: {
     static: {
