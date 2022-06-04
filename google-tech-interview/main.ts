77315b3164beba52649cf38b403fd10e4d322fc8
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-03 21:28:43 -0300

    [FEAT]: main.js renamed to main.ts

---
+++ google-tech-interview/main.ts
@@ -0,0 +1,13 @@
+// # O(n * log(N)) solution
+
+const form = document.getElementById('anagram-form');
+const firstWord = document.getElementById('first-word');
+const secondWord = document.getElementById('second-word');
+
+form.addEventListener('submit', () => {
+
+})
+
+// const sortWord(word) => {
+
+// }
