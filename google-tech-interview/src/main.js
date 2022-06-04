ec068842e1a477796181bff8048f12b67c941257
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-03 21:33:49 -0300

    minor changes.

---
+++ google-tech-interview/src/main.js
@@ -0,0 +1,8 @@
+// # O(n * log(N)) solution
+var form = document.getElementById('anagram-form');
+var firstWord = document.getElementById('first-word');
+var secondWord = document.getElementById('second-word');
+form.addEventListener('submit', function () {
+});
+// const sortWord(word) => {
+// }
