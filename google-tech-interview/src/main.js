ceb9861f775a0e162b9dae13a635195d7c905230
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-03 22:43:59 -0300

    [FEAT]: TS transpiled.

--- google-tech-interview/src/main.js
+++ google-tech-interview/src/main.js
@@ -1,8 +1,22 @@
-// # O(n * log(N)) solution
-var form = document.getElementById('anagram-form');
-var firstWord = document.getElementById('first-word');
-var secondWord = document.getElementById('second-word');
-form.addEventListener('submit', function () {
+// # O(n * log(n)) solution
+const form = document.getElementById('anagram-form');
+let firstWord = document.getElementById('firstWord');
+let secondWord = document.getElementById('secondWord');
+let result = document.querySelector('p#result span');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const firstOrdered = sortWord(formData.get('firstWord'));
+    const secondOrdered = sortWord(formData.get('secondWord'));
+    const answer = `"${firstWord.value}" and "${secondWord.value}" are`;
+    if (isAnagram(firstOrdered, secondOrdered)) {
+        result.innerHTML = ` YES! ${answer} anagrams!`;
+    }
+    else {
+        result.innerHTML = ` NO! ${answer} NOT anagrams!`;
+    }
+    firstWord.value = '';
+    secondWord.value = '';
 });
-// const sortWord(word) => {
-// }
+const sortWord = (word) => word.replaceAll(" ", "").toLowerCase().split("").sort().join("");
+const isAnagram = (word1, word2) => word1 === word2;
